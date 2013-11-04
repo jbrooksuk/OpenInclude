3a62ddeb3177ed8968b6b785d30506d80b8b6752
James Brooks <james@bluebaytravel.co.uk>
2013-11-04 15:05:17 +0000

    Working under ST3, I think

--- openinclude.py
+++ openinclude.py
@@ -5,7 +5,7 @@ import re
 class OpenInclude(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for region in self.view.sel():
-			syntax = self.view.syntax_name(region.begin())
+			syntax = self.view.scope_name(region.begin())
 			if re.match(".*string.quoted.double", syntax): self.select(self.view, region, '"') # Match & Select Doubles
 			if re.match(".*string.quoted.single", syntax): self.select(self.view, region, "'") # Match & Select Singles
 			
@@ -20,10 +20,10 @@ class OpenInclude(sublime_plugin.TextCommand):
 				self.view.window().open_file(filepath)
 				sublime.status_message("Opening file " + filepath)
 
-		def select(self, view, region, char):
-			begin = region.begin() - 1
-			end = region.begin()
-			while view.substr(begin) != char or view.substr(begin - 1) == '\\': begin -= 1
-			while view.substr(end) != char or view.substr(end - 1) == '\\': end += 1
-			view.sel().subtract(region)
-			view.sel().add(sublime.Region(begin + 1, end))
+	def select(self, view, region, char):
+		begin = region.begin() - 1
+		end = region.begin()
+		while view.substr(begin) != char or view.substr(begin - 1) == '\\': begin -= 1
+		while view.substr(end) != char or view.substr(end - 1) == '\\': end += 1
+		view.sel().subtract(region)
+		view.sel().add(sublime.Region(begin + 1, end))
